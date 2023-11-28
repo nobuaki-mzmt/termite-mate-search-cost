906a5fe7a468c0e761aeaa8544937994ceb6fc20
Nobuaki Mizumoto <71577022+nobuaki-mzmt@users.noreply.github.com>
2023-11-28 11:23:46 +0900

    simulations

---
+++ scripts/onesim.cpp
@@ -0,0 +1,69 @@
+#include <Rcpp.h>
+
+using namespace Rcpp;
+using namespace std;
+
+// Random sampling
+#include <random>
+#include <time.h>
+#include <cmath>
+
+// [[Rcpp::export]]
+NumericVector one_simulation(NumericVector x1, NumericVector y1,
+                             NumericVector x2, NumericVector y2, 
+                             double L, double detection) {
+  NumericVector endtime(1);
+  endtime[0]  = 1501;
+  double X1, Y1, X2, Y2, xdis, ydis, sepdis;
+  for(int i =0; i<1500; ++i){
+    X1 = fmod(x1[i], L);
+    Y1 = fmod(y1[i], L); 
+    X2 = fmod(x2[i], L); 
+    Y2 = fmod(y2[i], L); 
+    xdis = min(abs(X1 - X2), abs(L - abs(X1 - X2)));
+    ydis = min(abs(Y1 - Y2), abs(L - abs(Y1 - Y2)));
+    sepdis = sqrt(xdis*xdis + ydis*ydis);
+    if (sepdis <= detection) {
+      endtime[0] = i + 1;
+      break;
+    }
+  }
+  return endtime;
+}
+
+// [[Rcpp::export]]
+List randomize_trajectory(NumericVector x, NumericVector y) {
+  double rx, ry;
+  std::uniform_real_distribution<double> distribution(0.0, 1.0);
+  std::random_device seed_gen;
+  std::mt19937 engine(seed_gen());
+  auto rnd = [&] { return distribution(engine); };
+  
+  int rn1 = (rnd() < 0.5) ? -1 : 1;
+  int rn2 = (rnd() < 0.5) ? -1 : 1;
+  double theta = rnd()*3.1415926535*2;
+  double cos_theta = cos(theta);
+  double sin_theta = sin(theta);
+  
+  for(int i=1499; i>-1; --i){
+    x[i] = x[i] - x[0];
+    y[i] = y[i] - y[0];
+  }
+  
+  for(int i=0; i<1500; ++i){
+    // reflection
+    x[i] = x[i] * rn1;
+    y[i] = y[i] * rn2;
+
+    // rotation
+    rx = cos_theta * x[i] - sin_theta * y[i];
+    ry = sin_theta * x[i] + cos_theta * y[i];
+    x[i] = rx;
+    y[i] = ry;
+  }
+  Rcpp::List result;
+  result["x"] = x;
+  result["y"] = y;
+  return result;
+}
+
--- scripts/simulations.R
+++ scripts/simulations.R
@@ -1,43 +1,19 @@
 ## Data analysis on tired termite project
 ## N. Mizumoto
 
+
+require(Rcpp)
+
+rm(list = ls())
+
 ## Simulations
 load("data/df_all.rda")
+sourceCpp("scripts/onesim.cpp")
 
 ## Parameters
 L <- 223.6
 dis_encounter <- 7
-iter <- 10000
-
-sample_trajectory <- function(day, sex){
-  d_sub_pop <- subset(df_all, day==day & sex==sex)
-  
-  ids <- unique(d_sub_pop$id)
-  
-  sample_id   <- sample(ids, 1)
-  sample_time <- sample(1:6000, 1)
-  
-  d_sub <- subset(d_sub_pop, id == sample_id)[1:1500-1+sample_time,]
-  d_sub$time <- d_sub$time - d_sub$time[1]
-  d_sub$x <- d_sub$x - d_sub$x[1]
-  d_sub$y <- d_sub$y - d_sub$y[1]
-
-  # reflect
-  rn <- runif(1,0,1)
-  if(rn < 0.5){
-    d_sub$x <- -d_sub$x
-  }
-  if(rn < 0.5){
-    d_sub$y <- -d_sub$y
-  }
-  
-  # rotation
-  theta <- runif(1,0,1)*2*pi
-  rx <- cos(theta)*d_sub$x - sin(theta)*d_sub$y
-  ry <- sin(theta)*d_sub$x + cos(theta)*d_sub$y
-  df_temp <- data.frame(time = 1:1500, x=rx, y=ry)
-  return(df_temp)
-}
+iter <- 100
 
 one_simulation_1 <- function(df1, df2){
   PBC <- function(coord, L){
@@ -67,9 +43,6 @@ one_simulation_1 <- function(df1, df2){
     x2 <- PBC(x2 + x2_diff[i_time], L)
     y2 <- PBC(y2 + y2_diff[i_time], L)
     
-    #plot(x1, y1, xlim=c(0,L), ylim=c(0,L))
-    #points(x2, y2)
-    #Sys.sleep(0.1)
     
     xdis <- min(abs(x1 - x2), abs(L - abs(x1 - x2)))
     ydis <- min(abs(y1 - y2), abs(L - abs(y1 - y2)))
@@ -95,6 +68,10 @@ one_simulation_2 <- function(df1, df2){
   x2 <- x2 %% L
   y2 <- y2 %% L
   
+  #plot(x1, y1, xlim=c(0,L), ylim=c(0,L), type="l")
+  #points(x2, y2, type="l", col=2)
+  #Sys.sleep(0.1)
+  
   xdis <- apply(cbind(abs(x1 - x2), abs(L - abs(x1 - x2))), 1, min)
   ydis <- apply(cbind(abs(y1 - y2), abs(L - abs(y1 - y2))), 1, min)
   dis <-  sqrt(xdis^2 + ydis*2)
@@ -107,22 +84,54 @@ one_simulation_2 <- function(df1, df2){
   return(encounter_time)
 }
   
+one_simulation_3 <- function(df1, df2){
+  # initialize
+  x1 <- df1$x - df1$x[1]
+  y1 <- df1$y - df1$y[1]
+  x2 <- df2$x - df2$x[1] + runif(1,0,1)*L
+  y2 <- df2$y - df2$y[1] + runif(1,0,1)*L
+  
+  encounter_time = one_simulation(x1, y1, x2, y2, L, dis_encounter)
+  
+  return(encounter_time)
+}
+
+
 df_sim <- NULL
 for(i_day in 0:3){
   sex_list <- c("F", "M")
   for(i_sex in 1:2){
     res_vec <- rep(0, iter)
+    d_searcher <- subset(df_all, day==i_day & sex==sex_list[i_sex])
+    d_partner  <- subset(df_all, day==0 & sex==sex_list[3-i_sex])
+    
+    ids <- unique(d_searcher$id)
+    sample_id_s   <- sample(ids, iter, replace = T)
+    sample_time_s <- sample(1:6000, iter, replace = T)
+    
+    ids <- unique(d_partner$id)
+    sample_id_p   <- sample(ids, iter, replace = T)
+    sample_time_p <- sample(1:6000, iter, replace = T)
+    
     for(i_rep in 1:iter){
       print(paste("Sim: sex:",sex_list[i_sex], "day:", i_day, "iter:", i_rep))
-      df1 <- sample_trajectory(i_day, sex_list[i_sex])
-      df2 <- sample_trajectory(0, sex_list[2-i_sex])
-      res_vec[i_rep] <- one_simulation_2(df1, df2)
+      
+      d_sub_s <- subset(d_searcher, id == sample_id_s[i_rep])[1:1500-1+sample_time_s[i_rep],]
+      result <- randomize_trajectory(d_sub_s$x, d_sub_s$y)
+      df1 <- data.frame(time = 1:1500, x=result$x, y=result$y)
+      
+      d_sub_p <- subset(d_partner, id == sample_id_p[i_rep])[1:1500-1+sample_time_p[i_rep],]
+      result <- randomize_trajectory(d_sub_p$x, d_sub_p$y)
+      df2 <- data.frame(time = 1:1500, x=result$x, y=result$y)
+
+      res_vec[i_rep] <- one_simulation_3(df1, df2)
     }
     df_temp <- data.frame(
       day = i_day, sex = sex_list[i_sex], encounter_time = res_vec
     )
     df_sim <- rbind(df_sim, df_temp)
   }
+  save(df_sim, file="data/df_sim.rda")
 }
 
 
@@ -132,10 +141,10 @@ df_sim$encounter_time[is.na(df_sim$encounter_time)] <- 1500
 
 library("survminer")
 library(survival)
-df<-survfit(Surv(encounter_time)~day, type = "kaplan-meier", data=df_sim)
+df<-survfit(Surv(encounter_time/5)~day, type = "kaplan-meier", data=df_sim)
 ggsurvplot(fit = df, data = df_sim,
            pval = F, pval.method = TRUE,
            risk.table = F, conf.int = FALSE,
            ncensor.plot = FALSE, size = 1, 
-           xlab="Time (min)", ggtheme = theme_bw()  + theme(aspect.ratio = 0.75),
+           xlab="Time (sec)", ggtheme = theme_bw()  + theme(aspect.ratio = 0.75),
            facet.by = "sex")
